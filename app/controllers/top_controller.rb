69f2b08ca96548b2e7bf228487245f6b9c5bfd7c
柳澤燎汰 <yanagisawa@yanagisawaniwabinoMacBook-Pro.local>
2024-12-25 09:47:41 +0900

    アンケート機能追加

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,5 +1,5 @@
 class TopController < ApplicationController
-  skip_before_action :require_login, only: [ :index ]
+  skip_before_action :require_login, only: [ :index, :survey ]
 
   def index
   end

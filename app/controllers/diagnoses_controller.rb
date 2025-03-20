8496f8d65dcecc123dad3c950a1edbc284e6754b
柳澤燎汰 <yanagisawa@yanagisawaniwabinoMacBook-Pro.local>
2025-03-20 18:41:29 +0900

    設問数増加・診断機能拡張

--- app/controllers/diagnoses_controller.rb
+++ app/controllers/diagnoses_controller.rb
@@ -1,14 +1,14 @@
 class DiagnosesController < ApplicationController
-  skip_before_action :require_login, only: [:survey, :answer, :create, :show]
+  skip_before_action :require_login, only: [ :survey, :answer, :create, :show ]
 
   def survey
     @questions = [
-      { id: 1, text: "お好みのサウナのアツさは？", options: ["ゲキアツ", "アツい", "普通", "低め"] },
-      { id: 2, text: "外気浴でゆっくりしたい？", options: ["はい", "いいえ"] },
-      { id: 3, text: "水風呂は冷たいのが好み？", options: ["はい", "いいえ"] },
-      { id: 4, text: "サウナ施設 or スーパー銭湯", options: ["サウナ施設", "スーパー銭湯"] },
-      { id: 5, text: "古き良き雰囲気 or 新しく綺麗な感じ", options: ["古き良き", "新しく綺麗"] },
-      { id: 6, text: "性別を選択してください", options: ["男性", "女性"] },
+      { id: 1, text: "お好みのサウナのアツさは？", options: [ "ゲキアツ", "アツい", "普通", "低め" ] },
+      { id: 2, text: "外気浴でゆっくりしたい？", options: [ "はい", "いいえ" ] },
+      { id: 3, text: "水風呂は冷たいのが好み？", options: [ "はい", "いいえ" ] },
+      { id: 4, text: "サウナ施設 or スーパー銭湯", options: [ "サウナ施設", "スーパー銭湯" ] },
+      { id: 5, text: "古き良き雰囲気 or 新しく綺麗な感じ", options: [ "古き良き", "新しく綺麗" ] },
+      { id: 6, text: "性別を選択してください", options: [ "男性", "女性" ] },
       { id: 7, text: "お住まいの地域を入力してください" }
     ]
 
@@ -21,8 +21,8 @@ class DiagnosesController < ApplicationController
   end
 
   def answer
-    session[:answers] ||= {}
-    session[:answers][params[:question_id]] = params[:answer]
+    session[ :answers ] ||= {}
+    session[ :answers ][params[:question_id]] = params[ :answer ]
 
     next_question_id = params[:question_id].to_i + 1
 
@@ -34,11 +34,11 @@ class DiagnosesController < ApplicationController
   end
 
   def create
-    session[:answers][params[:question_id]] = params[:answer]
+    session[ :answers ][params[:question_id]] = params[ :answer ]
     Rails.logger.debug "最終的な受信した回答: #{session[:answers].inspect}"
   
     # 入力チェック：全て空 or location未記入の場合は no_result を返す
-    if session[:answers].values.all?(&:blank?) || session[:answers]["7"].blank?
+    if session[ :answers ].values.all?(&:blank?) || session[ :answers ]["7"].blank?
       redirect_to diagnosis_path(id: 0, no_result: true) and return
     end
   

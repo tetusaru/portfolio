f93560fe1fd2e7d83a99c6e2324cd0db344213f4
柳澤燎汰 <yanagisawa@yanagisawaniwabinoMacBook-Pro.local>
2025-03-22 03:12:52 +0900

    Mysauna登録機能実装、マイページ作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,10 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 255 }
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { password.present? } # password_confirmationを確認
+
+  # テスト用のヘルパーとして追加
+  def self.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end

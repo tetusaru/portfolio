1fbff5b6a826a63a7b206fa109bb216324100ada
柳澤燎汰 <yanagisawa@yanagisawaniwabinoMacBook-Pro.local>
2025-03-22 16:44:16 +0900

    Mysauna登録機能実装、マイページ作成

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,10 +11,9 @@ module ActiveSupport
     fixtures :all
 
     # Add more helper methods to be used by all tests here...
-    def log_in_as(user, password: "password")
-      post login_path, params: { session: { email: user.email, password: password } }, headers: { "HTTP_REFERER" => "/" }
-      puts "ログイン結果: user_id = #{session[:user_id].inspect}" # 追加：sessionの中身を確認
-      puts "レスポンス status: #{response.status}" # 追加：リダイレクト or OKか確認
+    def log_in_as(user, password: 'password')
+      post login_path, params: { session: { email: user.email, password: password } }
+      puts "session[:user_id] after login = #{session[:user_id].inspect}"
       follow_redirect! if response.redirect?
     end
   end

1e652f2734c2c8c2cfd7c88ff34fb4835f52888b
柳澤燎汰 <yanagisawa@yanagisawaniwabinoMacBook-Pro.local>
2025-03-22 18:30:51 +0900

    Mysauna登録機能実装、マイページ作成

--- test/controllers/mysaunas_controller_test.rb
+++ test/controllers/mysaunas_controller_test.rb
@@ -6,29 +6,29 @@ class MysaunasControllerTest < ActionDispatch::IntegrationTest
     @user.mysauna&.destroy
     log_in_as(@user)
   end
-
+  
   test "should get new" do
-    get new_mysauna_url
+    get mysaunas_new_path
     assert_response :success
   end
-
-  test "should create mysauna" do
-    post mysaunas_url, params: { mysauna: { sauna_name: "Test Sauna", comment: "Great place!" } }
-    assert_response :redirect
-  end
-
+  
   test "should get edit" do
-    get edit_mysauna_url(@mysauna.id)
+    @mysauna = @user.create_mysauna(sauna_name: "テストサウナ", comment: "テストコメント")
+    get edit_mysauna_path(@mysauna)
     assert_response :success
   end
-
+  
   test "should update mysauna" do
-    patch mysauna_url(@mysauna.id), params: { mysauna: { sauna_name: "Updated Sauna" } }
-    assert_response :redirect
+    @mysauna = @user.create_mysauna(sauna_name: "テストサウナ", comment: "テストコメント")
+    patch mysauna_path(@mysauna), params: { mysauna: { sauna_name: "更新サウナ" } }
+    assert_redirected_to mypage_user_path(@user)
   end
-
+  
   test "should destroy mysauna" do
-    delete mysauna_url(@mysauna.id)
-    assert_response :redirect
+    @mysauna = @user.create_mysauna(sauna_name: "テストサウナ", comment: "テストコメント")
+    assert_difference("Mysauna.count", -1) do
+      delete mysauna_path(@mysauna)
+    end
+    assert_redirected_to mypage_user_path(@user)
   end
 end

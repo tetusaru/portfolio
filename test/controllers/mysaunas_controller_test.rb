25c5a0927ae1b452b4034e79ae96205b6bea342f
柳澤燎汰 <yanagisawa@yanagisawaniwabinoMacBook-Pro.local>
2025-03-22 18:32:40 +0900

    Mysauna登録機能実装、マイページ作成

--- test/controllers/mysaunas_controller_test.rb
+++ test/controllers/mysaunas_controller_test.rb
@@ -6,24 +6,24 @@ class MysaunasControllerTest < ActionDispatch::IntegrationTest
     @user.mysauna&.destroy
     log_in_as(@user)
   end
-  
+
   test "should get new" do
     get mysaunas_new_path
     assert_response :success
   end
-  
+
   test "should get edit" do
     @mysauna = @user.create_mysauna(sauna_name: "テストサウナ", comment: "テストコメント")
     get edit_mysauna_path(@mysauna)
     assert_response :success
   end
-  
+
   test "should update mysauna" do
     @mysauna = @user.create_mysauna(sauna_name: "テストサウナ", comment: "テストコメント")
     patch mysauna_path(@mysauna), params: { mysauna: { sauna_name: "更新サウナ" } }
     assert_redirected_to mypage_user_path(@user)
   end
-  
+
   test "should destroy mysauna" do
     @mysauna = @user.create_mysauna(sauna_name: "テストサウナ", comment: "テストコメント")
     assert_difference("Mysauna.count", -1) do

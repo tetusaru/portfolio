77437fbd9f838a9b73c9a3d845cebe1518266038
柳澤燎汰 <yanagisawa@yanagisawaniwabinoMacBook-Pro.local>
2025-04-06 23:53:11 +0900

    マイグレーションファイル修正・作成

--- db/migrate/20250406133004_create_all_solid_queue_tables.rb
+++ db/migrate/20250406133004_create_all_solid_queue_tables.rb
@@ -73,12 +73,9 @@ class CreateAllSolidQueueTables < ActiveRecord::Migration[7.1]
     create_table :solid_queue_ready_executions, if_not_exists: true do |t|
       t.bigint :job_id, null: false
       t.string :queue_name, null: false
-      t.integer :priority, default: 0, null: false
       t.datetime :created_at, null: false
 
       t.index :job_id, unique: true
-      t.index [ :priority, :job_id ], name: "index_solid_queue_poll_all"
-      t.index [ :queue_name, :priority, :job_id ], name: "index_solid_queue_poll_by_queue"
     end
 
     create_table :solid_queue_recurring_executions, if_not_exists: true do |t|

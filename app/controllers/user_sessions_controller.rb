44cf0b189c33c93df51ddc06b354637027bbe77e
柳澤燎汰 <yanagisawa@yanagisawaniwabinoMacBook-Pro.local>
2025-04-10 02:35:29 +0900

    オートログイン機能実装

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -6,7 +6,7 @@ class UserSessionsController < ApplicationController
   def create
     @user = login(params[:email], params[:password], params[:remember_me] == "1")
     puts "login result @user = #{@user.inspect}"
-  
+
     if @user
       redirect_to root_path, notice: "ログインされました！"
     else

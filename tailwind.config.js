66f1a7554ef78c1859a4167529c31d52a19bf03b
柳澤燎汰 <yanagisawa@yanagisawaniwabinoMacBook-Pro.local>
2025-04-14 18:34:48 +0900

    各画面レスポンシブ対応

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,24 @@ module.exports = {
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
-    './app/javascript/**/*.js'
-  ]
+    './app/javascript/**/*.js',
+  ],
+  safelist: [
+    'min-h-[80px]',
+    'min-h-[120px]',
+    'py-16',
+    'py-8',
+    'px-4',
+    'bg-blue-500',
+    'bg-red-500',
+    'bg-yellow-500',
+    'text-white',
+    'flex',
+    'justify-between',
+    'items-center',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 }

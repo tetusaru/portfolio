e7b9cc2b4711c5a39b3791c949068c45b61cbee8
柳澤燎汰 <yanagisawa@yanagisawaniwabinoMacBook-Pro.local>
2025-06-19 11:03:06 +0900

    TOP画面の修正

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,7 +20,17 @@ module.exports = {
     'items-center',
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        blink: 'blink 1.5s step-start infinite',
+      },
+      keyframes: {
+        blink: {
+          '0%, 50%, 100%': { opacity: 1 },
+          '25%, 75%': { opacity: 0 },
+        },
+      },
+    },
   },
   plugins: [],
 }

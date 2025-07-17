c5091903f73e7a8ac27ea52ab27818c588d1402b
柳澤燎汰 <yanagisawa@yanagisawaniwabinoMacBook-Pro.local>
2025-07-17 10:37:52 +0900

    不要ファイル削除

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,22 +1,7 @@
 class TopController < ApplicationController
-  skip_before_action :require_login, only: [ :index, :survey, :survey_step2 ]
+  skip_before_action :require_login, only: [ :index ]
 
   def index
   end
 
-  def survey
-    @questions_page1 = [
-      { id: 1, text: "熱いサウナが好き？" },
-      { id: 2, text: "外気浴でゆっくりしたい？" },
-      { id: 3, text: "水風呂は冷たいのが好み？" }
-    ]
-  end
-
-  def survey_step2
-    @questions_page2 = [
-      { id: 4, text: "性別は？" },
-      { id: 5, text: "どこのサウナを探している？" }
-    ]
-    @previous_answers = params[:answers] || {}
-  end
 end
